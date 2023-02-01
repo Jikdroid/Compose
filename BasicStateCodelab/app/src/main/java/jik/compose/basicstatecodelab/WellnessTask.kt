e3c68447dbfe021ecef8185bae2d829e39296187
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2023-02-02 02:54:23 +0900

    #3 - State in ViewModel

--- BasicStateCodelab/app/src/main/java/jik/compose/basicstatecodelab/WellnessTask.kt
+++ BasicStateCodelab/app/src/main/java/jik/compose/basicstatecodelab/WellnessTask.kt
@@ -1,6 +1,13 @@
 package jik.compose.basicstatecodelab
 
-data class WellnessTask(
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+class WellnessTask(
     val id: Int,
     val label: String,
-)
+    initialChecked: Boolean = false
+) {
+    var checked by mutableStateOf(initialChecked)
+}
